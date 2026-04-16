2e601dd717f0a45d61b83bbc3710b58aaf8ce5b0
TemirovArtem <409670@niuitmo.ru>
2026-04-16 21:54:24 +0300

    MeshLoader

--- src/Resources/ResourceManager.h
+++ src/Resources/ResourceManager.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Resource.h"
+#include "Logger.h"
 #include <unordered_map>
 #include <memory>
 #include <string>
@@ -7,10 +8,11 @@
 #include <mutex>
 #include <typeindex>
 #include <any>
+#include <algorithm>
 
-// Cache type: map<path, weak_ptr<Resource<T>>>
+// Cache type: map<path, shared_ptr<Resource<T>>>
 template<typename T>
-using ResourceCache = std::unordered_map<std::string, std::weak_ptr<Resource<T>>>;
+using ResourceCache = std::unordered_map<std::string, std::shared_ptr<Resource<T>>>;
 
 // Loader list type
 template<typename T>
@@ -47,7 +49,12 @@ public:
     void unloadAllResources();
 
     template<typename T>
-    size_t getCacheSize() const;
+    size_t getCacheSize() const
+    {
+        std::lock_guard<std::mutex> lock(mutex);
+        const auto& cache = const_cast<ResourceManager*>(this)->getCache<T>();
+        return cache.size();
+    }
 
 private:
     ResourceManager() = default;
@@ -63,5 +70,150 @@ private:
     std::unordered_map<std::type_index, std::any> caches;
     std::unordered_map<std::type_index, std::any> loaders;
 };
+
+template<typename T>
+std::shared_ptr<Resource<T>> ResourceManager::load(const std::string& path)
+{
+    {
+        std::lock_guard<std::mutex> lock(mutex);
+        auto& cache = getCache<T>();
+
+        auto it = cache.find(path);
+        if (it != cache.end())
+        {
+            auto resource = it->second;
+            if (resource)
+            {
+                LOG_RESOURCEMANAGER(std::string("Cache hit: ") + path);
+                return resource;
+            }
+            else
+            {
+                cache.erase(it);
+            }
+        }
+
+        LOG_RESOURCEMANAGER(std::string("Loading: ") + path);
+
+        std::unique_ptr<T> data;
+
+        auto& loaders = getLoaders<T>();
+        if (loaders.empty())
+        {
+            LOG_ERROR(std::string("No loader registered for type: ") + std::string(typeid(T).name()));
+            return nullptr;
+        }
+
+        // Use the highest priority loader (first in sorted list)
+        data = loaders.front().second(path);
+
+        if (!data) 
+        {
+            LOG_ERROR(std::string("Failed to load: ") + path);
+            return nullptr;
+        }
+
+        auto resource = std::make_shared<Resource<T>>(path, std::move(data));
+        cache[path] = resource;
+
+        return resource;
+    }
+}
+
+template<typename T>
+void ResourceManager::registerLoader(std::function<std::unique_ptr<T>(const std::string&)> loader, int priority)
+{
+    std::lock_guard<std::mutex> lock(mutex);
+    auto& loaders = getLoaders<T>();
+    loaders.push_back({ priority, loader });
+
+    std::sort(loaders.begin(), loaders.end(),
+        [](const auto& a, const auto& b) { return a.first < b.first; });
+
+    LOG_RESOURCEMANAGER(std::string("Loader registered for type: ") + std::string(typeid(T).name()));
+}
+
+template<typename T>
+std::shared_ptr<Resource<T>> ResourceManager::get(const std::string& path)
+{
+    std::lock_guard<std::mutex> lock(mutex);
+
+    auto& cache = getCache<T>();
+    auto it = cache.find(path);
+    if (it != cache.end()) 
+    {
+        auto resource = it->second;
+        if (resource) 
+        {
+            return resource;
+        }
+        else 
+        {
+            cache.erase(it);
+        }
+    }
+
+    return nullptr;
+}
+
+template<typename T>
+bool ResourceManager::isLoaded(const std::string& path)
+{
+    std::lock_guard<std::mutex> lock(mutex);
+
+    auto& cache = getCache<T>();
+    auto it = cache.find(path);
+    return it != cache.end() && it->second != nullptr;
+}
+
+template<typename T>
+void ResourceManager::unload(const std::string& path)
+{
+    std::lock_guard<std::mutex> lock(mutex);
+
+    auto& cache = getCache<T>();
+    auto it = cache.find(path);
+    if (it != cache.end()) 
+    {
+        cache.erase(it);
+
+        LOG_RESOURCEMANAGER(std::string("Unloaded: ") + std::string(path));
+    }
+}
+
+template<typename T>
+void ResourceManager::unloadAll()
+{
+    std::lock_guard<std::mutex> lock(mutex);
+
+    auto& cache = getCache<T>();
+    size_t count = cache.size();
+    cache.clear();
+
+    LOG_RESOURCEMANAGER(std::string("Unloaded ") + std::to_string(count) + std::string(" resources of type: ") + std::string(typeid(T).name()));
+}
+
+template<typename T>
+ResourceCache<T>& ResourceManager::getCache()
+{
+    std::type_index typeIdx = std::type_index(typeid(T));
+    auto it = caches.find(typeIdx);
+    if (it == caches.end()) {
+        caches[typeIdx] = ResourceCache<T>();
+    }
+    return std::any_cast<ResourceCache<T>&>(caches[typeIdx]);
+}
+
+template<typename T>
+LoaderList<T>& ResourceManager::getLoaders()
+{
+    std::type_index typeIdx = std::type_index(typeid(T));
+    auto it = loaders.find(typeIdx);
+    if (it == loaders.end()) 
+    {
+        loaders[typeIdx] = LoaderList<T>();
+    }
+    return std::any_cast<LoaderList<T>&>(loaders[typeIdx]);
+}
 
 #define RESOURCE_MANAGER ResourceManager::getInstance()
